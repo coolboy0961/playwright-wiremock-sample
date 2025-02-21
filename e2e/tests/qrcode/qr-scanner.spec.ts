3e9e6fec15b7519fbe569699a12247aca8713613
jiadchen <jiadchen@redhat.com>
2025-02-21 16:38:59 +0900

    カメラスキャンの仮想化をした上でテストケースを作成した。

---
+++ e2e/tests/qrcode/qr-scanner.spec.ts
@@ -0,0 +1,40 @@
+import { test, expect, type Page } from "@playwright/test";
+import { WrapperPage } from "../pages/wrapper-page";
+
+const baseUrl = "http://localhost:4200";
+
+test.beforeEach(async ({ page }) => {
+  const wrapperPage = new WrapperPage(page, baseUrl);
+  await wrapperPage.goto();
+});
+
+test.use({
+  launchOptions: {
+    args: [
+      "--use-fake-ui-for-media-stream",
+      "--use-fake-device-for-media-stream",
+      // 画面録画で作った画像ファイルをffmpegでmjpegに変換したものを使う必要がある
+      // ffmpeg -i qrcode.mov -vcodec mjpeg -q:v 2 -an qrcode.mjpeg
+      "--use-file-for-fake-video-capture=tests/qrcode/qrcode.mjpeg",
+    ],
+  },
+  permissions: ['camera'],
+});
+
+test.describe("Scan", () => {
+  test.only("QRコードスキャナーが表示されていることを確認", async ({
+    page,
+  }) => {
+    // Arrange
+    const expectedScanedText = "https://www.google.com"
+
+    // Act
+    // 自動的にスキャンが始まるため、何もしない
+
+    // Assert
+    // 「QRコードスキャナー」のテキストが表示されているか確認
+    await expect(page.getByText("QRコードスキャナー")).toBeVisible();
+    // スキャンしたテキストが表示されているか確認
+    await expect(page.getByText(expectedScanedText)).toBeVisible({ timeout: 30000 });
+  });
+});
